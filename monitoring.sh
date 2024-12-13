6dfe6dd6f97ea5278ee6792be2f6fd5e1cb73446
youness anflous <anayouness51@gmail.com>
2024-12-13 19:09:24 +0100

    monitoring script

--- monitoring.sh
+++ monitoring.sh
@@ -2,12 +2,16 @@
 
 echo "hello guys!" | wall
 
+# Variables command:
 ARCH=$(uname -a)
 P_CPU=$(lscpu | grep -i "Socket(s)" | awk '{print $2}')
-V_CPU=$()
+V_CPU=$(lscpu | grep -i "CPU(s)" | awk '{print $2}')
+Usage_Memory=free #not valid
+MAC=$(hostname -I && ip link show | grep -i ether | awk '{print $2}') #-> IP AND MAC ADDRESS "()"
 
-MAC=$(ip link show | grep -i ether | awk '{print $2}') #-> IP AND MAC ADDRESS
-
-echo $ARCH
-echo $P_CPU
-echo $MAC
+# excute the command:
+echo "Architecture:" $ARCH
+echo "CPU physical:" $P_CPU
+echo "vCPU:" $V_CPU
+echo "Memory Usage:"
+echo "Network:" $MAC
