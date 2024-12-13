258ecbe710a56d74bc54de78572f3b4747320eeb
youness anflous <anayouness51@gmail.com>
2024-12-13 15:36:49 +0100

    monitoring script

--- monitoring.sh
+++ monitoring.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "hello this is a test"
+echo "hello guys!" | wall
+
+ARCH=$(uname -a)
+P_CPU=$(lscpu | grep -i "Socket(s)" | awk '{print $2}')
+V_CPU=$()
+
+MAC=$(ip link show | grep -i ether | awk '{print $2}') #-> IP AND MAC ADDRESS
+
+echo $ARCH
+echo $P_CPU
+echo $MAC
