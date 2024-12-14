15bdaad6890226f664046f109db69acbf40869f0
Younes Anflous <yanflous@e2r5p11.1337.ma>
2024-12-14 18:38:14 +0100

    monitoring script update

--- monitoring.sh
+++ monitoring.sh
@@ -9,18 +9,21 @@ Physical_CPU=$(lscpu | grep -i "Socket(s)" | awk '{print $2}') # done
 Virtual_CPU=$(lscpu | grep -i "CPU(s)" | awk '{print $2}') # check
 Usage_Memory=$(free --mega | grep -i "mem" | awk '{print $3}') # done
 Total_Memory=$(free --mega | grep -i "mem" | awk '{print $2}') # done
-
-Per=$(echo "$Usage_Memory $Total_Memory" | awk '{printf "%.2f", ($1 / $2) * 100}')
-
+Percentage=$(echo "$Usage_Memory $Total_Memory" | awk '{printf "%.2f", ($1 / $2) * 100}')
+#
+Disk_Total=$(df -BG | grep "^/dev" | awk '{sum += $2} END {print sum "G"}')
+#Disk_Usage=$()
+#
 IP_Addr=$(echo "IP $(hostname -I)")
 MAC_Addr=$(ip link | grep -i ether | awk '{print $2}')
 #
 # excute the command:
 #
-echo "Architecture: ${Architecture}" # done
-echo "CPU physical: ${Physical_CPU}" # done
-echo "vCPU: ${Virtual_CPU}" # check
-echo "Memory Usage: ${Usage_Memory}/${Total_Memory}MB (${Per}%)" # done
+echo "#Architecture: ${Architecture}" # done
+echo "#CPU physical: ${Physical_CPU}" # done
+echo "#vCPU: ${Virtual_CPU}" # check
+echo "#Memory Usage: ${Usage_Memory}/${Total_Memory}MB (${Percentage}%)" # done
 
+echo "#Disk Usage: ${Disk_Total}" # work on it
 
-echo "Network: ${IP_Addr} (${MAC_Addr})"
+echo "#Network: ${IP_Addr} (${MAC_Addr})" # check
