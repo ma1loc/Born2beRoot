ea8ff85cabdabe9ba7f1cd8ed7deb49ff2848c2d
Younes Anflous <yanflous@e2r5p11.1337.ma>
2024-12-14 18:57:59 +0100

    monitoring script update

--- monitoring.sh
+++ monitoring.sh
@@ -7,13 +7,12 @@ echo "hello guys!" | wall # brodcasting message to all in the server!!!!d
 Architecture=$(uname -a) # done
 Physical_CPU=$(lscpu | grep -i "Socket(s)" | awk '{print $2}') # done
 Virtual_CPU=$(lscpu | grep -i "CPU(s)" | awk '{print $2}') # check
-Usage_Memory=$(free --mega | grep -i "mem" | awk '{print $3}') # done
-Total_Memory=$(free --mega | grep -i "mem" | awk '{print $2}') # done
-Percentage=$(echo "$Usage_Memory $Total_Memory" | awk '{printf "%.2f", ($1 / $2) * 100}')
-#
-Disk_Total=$(df -BG | grep "^/dev" | awk '{sum += $2} END {print sum "G"}')
-#Disk_Usage=$()
-#
+Memory_Usage=$(free --mega | grep -i "mem" | awk '{print $3}') # done
+Memory_Total=$(free --mega | grep -i "mem" | awk '{print $2}') # done
+Memory_Percentage=$(echo "$Usage_Memory $Total_Memory" | awk '{printf "%.2f", ($1 / $2) * 100}')
+Disk_Total=$(df -BG | grep "^/dev" | awk '{total += $2} END {print total "Gb"}')
+Disk_Usage=$(df -m | grep "^/dev" | awk '{usage += $3} END {print usage}')
+Disk_Percentage=$(echo "$Disk_Usage $Total_Memory")
 IP_Addr=$(echo "IP $(hostname -I)")
 MAC_Addr=$(ip link | grep -i ether | awk '{print $2}')
 #
@@ -22,8 +21,8 @@ MAC_Addr=$(ip link | grep -i ether | awk '{print $2}')
 echo "#Architecture: ${Architecture}" # done
 echo "#CPU physical: ${Physical_CPU}" # done
 echo "#vCPU: ${Virtual_CPU}" # check
-echo "#Memory Usage: ${Usage_Memory}/${Total_Memory}MB (${Percentage}%)" # done
+echo "#Memory Usage: ${Memory_Usage}/${Memory_Total}MB (${Percentage}%)" # done
 
-echo "#Disk Usage: ${Disk_Total}" # work on it
+echo "#Disk Usage: ${Disk_Usage}/${Disk_Total}" # work on it
 
 echo "#Network: ${IP_Addr} (${MAC_Addr})" # check
