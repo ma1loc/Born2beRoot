95b218abafa2098d964b69e176e972ccc1afdeec
youness anflous <anayouness51@gmail.com>
2024-12-15 16:52:51 +0100

    monitoring script update

--- monitoring.sh
+++ monitoring.sh
@@ -5,8 +5,8 @@ echo "hello guys!" | wall # brodcasting message to all in the server!!!!d
 # Variables command:
 #
 Architecture=$(uname -a) # done
-Physical_CPU=$(lscpu | grep -i "Socket(s)" | awk '{print $2}') # done
-Virtual_CPU=$(lscpu | grep -i "CPU(s)" | awk '{print $2}') # check
+Physical_CPU=$(lscpu | grep -i "socket(s)" | awk '{print $2}') # done
+Virtual_CPU=$(lscpu | grep -i "cpu(s)" | awk '{print $2}') # check
 Memory_Usage=$(free --mega | grep -i "mem" | awk '{print $3}') # done
 Memory_Total=$(free --mega | grep -i "mem" | awk '{print $2}') # done
 Memory_Percentage=$(echo "$Memory_Usage $Memory_Total" | awk '{printf "%.2f", ($1 / $2) * 100}')
@@ -15,7 +15,7 @@ Disk_Usage=$(df -m | grep -i "^/dev" | awk '{usage += $3} END {print usage}')  #
 Disk_Total_In_M=$(df -m | grep -i "^/dev" | awk '{total_m += $2} END {print total_m}') # done
 Disk_Percentage=$(echo "$Disk_Usage $Disk_Total_In_M" | awk '{printf "%d", ($1 / $2) * 100}') # done
 
-CPU_Load=$(top -n1 | grep -i "Cpu(s)" | )
+CPU_Load=$(top -n1 | grep -i "cpu(s)" | awk '{print $}')
 
 IP_Addr=$(echo "IP $(hostname -I)")
 MAC_Addr=$(ip link | grep -i "ether" | awk '{print $2}')
@@ -27,5 +27,5 @@ echo "#CPU physical: ${Physical_CPU}" # done
 echo "#vCPU: ${Virtual_CPU}" # check
 echo "#Memory Usage: ${Memory_Usage}/${Memory_Total}MB (${Memory_Percentage}%)" # done
 echo "#Disk Usage: ${Disk_Usage}/${Disk_Total}Gb (${Disk_Percentage}%)" # done no
-echo "#CPU load: " # working on it.
+echo "#CPU load: ${CPU_Load}" # working on it.
 echo "#Network: ${IP_Addr} (${MAC_Addr})" # check.
