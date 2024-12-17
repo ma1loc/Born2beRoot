3794c139eeb5ca00c0e95963bd688ff877f8f624
Younes Anflous <yanflous@e2r5p11.1337.ma>
2024-12-17 09:49:10 +0100

    monitoring script update

--- monitoring.sh
+++ monitoring.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-Architecture=$(uname -a) # done
-Physical_CPU=$(lscpu | grep "Socket(s)" | awk '{print $2}') # done
-Virtual_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}') # done
-Memory_Usage=$(free --mega | grep -i "mem" | awk '{print $3}') # done
-Memory_Total=$(free --mega | grep -i "mem" | awk '{print $2}') # done
-Memory_Percentage=$(echo "$Memory_Usage $Memory_Total" | awk '{printf "%.2f", ($1 / $2) * 100}') # done
-Disk_Total=$(df -BG | grep "^/dev" | awk '{total += $2} END {print total}') # done
-Disk_Usage=$(df -m | grep "^/dev" | awk '{usage += $3} END {print usage}')  # done
-Disk_Total_In_M=$(df -m | grep "^/dev" | awk '{total_m += $2} END {print total_m}') # done
-Disk_Percentage=$(echo "$Disk_Usage $Disk_Total_In_M" | awk '{printf "%d", ($1 / $2) * 100}') # done
-CPU_Load=$(mpstat | grep "all" | awk '{printf "%.1f", (100 - $13)}') # fixed top
-Last_Boot=$(who -b | awk '{print $3 " " $4}') # done
-LVM_use=$(lsblk | grep -q "lvm" && echo "yes" || echo "no") # fixed with fallback
-Connections_TCP=$(ss -Ht state ESTABLISHED | wc -l) # done
-User_log=$(users | wc -w) # done
-IP_Addr=$(hostname -I) # done
-MAC_Addr=$(ip link | grep -i "ether" | awk '{print $2}') # done
-Sudo=$(journalctl -q | grep "command" | wc -l) # done
+Architecture=$(uname -a)
+Physical_CPU=$(lscpu | grep "Socket(s)" | awk '{print $2}')
+Virtual_CPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
+Memory_Usage=$(free --mega | grep -i "mem" | awk '{print $3}')
+Memory_Total=$(free --mega | grep -i "mem" | awk '{print $2}')
+Memory_Percentage=$(echo "$Memory_Usage $Memory_Total" | awk '{printf "%.2f", ($1 / $2) * 100}')
+Disk_Total=$(df -BG | grep "^/dev" | awk '{total += $2} END {print total}')
+Disk_Usage=$(df -m | grep "^/dev" | awk '{usage += $3} END {print usage}')
+Disk_Total_In_M=$(df -m | grep "^/dev" | awk '{total_m += $2} END {print total_m}')
+Disk_Percentage=$(echo "$Disk_Usage $Disk_Total_In_M" | awk '{printf "%d", ($1 / $2) * 100}')
+CPU_Load=$(mpstat | grep "all" | awk '{printf "%.1f", (100 - $13)}')
+Last_Boot=$(who -b | awk '{print $3 " " $4}')
+LVM_use=$(lsblk | grep -q "lvm" && echo "yes" || echo "no")
+Connections_TCP=$(ss -Ht state ESTABLISHED | wc -l)
+User_log=$(users | wc -w)
+IP_Addr=$(hostname -I)
+MAC_Addr=$(ip link | grep -i "ether" | awk '{print $2}')
+Sudo=$(journalctl -q | grep "COMMAND" | wc -l)
 #
 # Execute the command:
 #
@@ -31,6 +31,6 @@ echo "#Last boot: ${Last_Boot}"
 echo "#LVM use: ${LVM_use}"
 echo "#Connections TCP: ${Connections_TCP} ESTABLISHED"
 echo "#User log: ${User_log}"
-echo "#Network: ${IP_Addr}(${MAC_Addr})"
+echo "#Network: IP ${IP_Addr}(${MAC_Addr})"
 echo "#Sudo : ${Sudo} cmd"
 
